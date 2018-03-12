46e9732d3979725627672cd02dd1e9e62966d7a2
hadrosaur <hadrosaur@google.com>
2018-03-12 10:06:17 -0700

    Added step 2 and step 3

---
+++ step01/app/src/main/java/com/google/sample/optimizedforchromeos/MainActivity.java
@@ -0,0 +1,95 @@
+/*      Copyright 2018 Google Inc. All rights reserved.
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+*/
+package com.google.sample.optimizedforchromeos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button sendButton = findViewById(R.id.button_send);
+        ImageView dinoImage1 = findViewById(R.id.image_dino1);
+        ImageView dinoImage2 = findViewById(R.id.image_dino2);
+        ImageView dinoImage3 = findViewById(R.id.image_dino3);
+        ImageView dinoImage4 = findViewById(R.id.image_dino4);
+
+        final EditText messageField = findViewById(R.id.edit_message);
+
+        final TextView messageCounter = findViewById(R.id.text_messages_sent);
+        final TextView imageClickCounter = findViewById(R.id.text_dino_clicks);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageCounter.setText(incrementStringInteger(messageCounter.getText().toString()));
+                messageField.getText().clear();
+            }
+        });
+
+        dinoImage1.setOnClickListener(new ImageOnClickListener(imageClickCounter));
+        dinoImage2.setOnClickListener(new ImageOnClickListener(imageClickCounter));
+        dinoImage3.setOnClickListener(new ImageOnClickListener(imageClickCounter));
+        dinoImage4.setOnClickListener(new ImageOnClickListener(imageClickCounter));
+
+        messageField.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent keyEvent) {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    sendButton.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    class ImageOnClickListener implements View.OnClickListener {
+        TextView mClickCounter;
+
+        public ImageOnClickListener(TextView mClickCounter) {
+            this.mClickCounter = mClickCounter;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mClickCounter.setText(incrementStringInteger(mClickCounter.getText().toString()));
+        }
+    }
+
+    static String incrementStringInteger(String oldCount) {
+        Integer count;
+
+        try {
+            count = Integer.parseInt(oldCount);
+        } catch (NumberFormatException e) {
+            return "-1";
+        }
+
+        count++;
+        return count.toString();
+    }
+}
