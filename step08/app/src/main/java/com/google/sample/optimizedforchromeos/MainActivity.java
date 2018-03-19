a9e2498aa165cbac3e39e1b5edd8e83f3a5c0edf
hadrosaur <hadrosaur@google.com>
2018-03-19 11:34:34 -0700

    Removed unnecesary v.invalidate lines in Drag and Drop

--- step08/app/src/main/java/com/google/sample/optimizedforchromeos/MainActivity.java
+++ step08/app/src/main/java/com/google/sample/optimizedforchromeos/MainActivity.java
@@ -289,7 +289,6 @@ public class MainActivity extends AppCompatActivity {
 
                         // Greenify background colour so user knows this is a target
                         v.setBackgroundColor(Color.argb(55,0,255,0));
-                        v.invalidate();
                         return true;
                     }
 
@@ -299,19 +298,16 @@ public class MainActivity extends AppCompatActivity {
                 case DragEvent.ACTION_DRAG_ENTERED:
                     // Increase green background colour when item is over top of target
                     v.setBackgroundColor(Color.argb(150,0,255,0));
-                    v.invalidate();
                     return true;
 
                 case DragEvent.ACTION_DRAG_EXITED:
                     // Less intense green background colour when item not over target
                     v.setBackgroundColor(Color.argb(55,0,255,0));
-                    v.invalidate();
                     return true;
 
                 case DragEvent.ACTION_DRAG_ENDED:
                     // Restore background colour to transparent
                     v.setBackgroundColor(Color.argb(0,255,255,255));
-                    v.invalidate();
                     return true;
 
                 case DragEvent.ACTION_DROP:
